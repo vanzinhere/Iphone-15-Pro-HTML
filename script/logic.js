7d837515ffb38232309f083b764123fc2072ea7b
vanzinhere <135933914+vanzinhere@users.noreply.github.com>
2025-05-11 17:51:12 +0700

    Add files via upload

---
+++ script/logic.js
@@ -0,0 +1,7 @@
+const power_button = document.getElementById("power-button");
+const dnd_button = document.getElementById("dnd-button");
+const volume_up_button = document.getElementById("volume-up-button");
+const volume_down_button_button = document.getElementById("volume-down-button");
+
+const dnd = false;
+const record = false;
